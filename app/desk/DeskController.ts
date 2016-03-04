824f297280da89e4edf4047093b0bb00a3f8336a
William Ngo <will@desk.com>
2016-03-04 10:30:05 -0800

    WIP: Start/stop a poller

--- app/desk/DeskController.ts
+++ app/desk/DeskController.ts
@@ -1,12 +1,15 @@
 
 export class DeskController {
-  constructor ($scope, DeskPoller) {
+  constructor ($scope, DeskPoller, $http) {
     $scope.foo = "hello world!!";
     
-    var poller = new DeskPoller(5000);
+    // Create a new poller, passing in a promise as the action to perform every interval.
+    var poller = new DeskPoller('JobsPoller',500,function fetchItem (lastModified:number) {
+            return $http.get('/app/data/changes.json?updatedAt='+lastModified);
+        });
     
     
-    var jobs = new DeskPoller('JobsPoller');
+    // var jobs = new DeskPoller('JobsPoller');
     // Observable configured to execute something at an interval.
     // BUT we never subscribe directly to jobs
     
@@ -14,9 +17,19 @@ export class DeskController {
     
 
     // PollerJob.subscribe()
-
-    poller.subscribe(data => {
-       console.log('returned data',data);
-    });
+    
+    var unsubscribe;
+    
+    $scope.start = function() {
+      console.log('starting');
+      unsubscribe = poller.subscribe(data => {
+        console.log('returned data',data);
+      });
+    }
+    
+    $scope.stop = function() {
+        console.log('stopping');
+        unsubscribe.dispose();
+    }
   };
 }
--- app/desk/DeskPoller.ts
+++ app/desk/DeskPoller.ts
@@ -9,39 +9,34 @@ import {Observable} from 'rx';
 // 5. Exponential Backoff
 
 
-
-
 // $http will be injected with Angular
 export const DeskPoller = function ($http) {
   return class DeskPoller {
-    
-    constructor (frequency: number, url: string) {
-        function fetchItem (lastModified) {
-            return Observable.fromPromise($http.get('/app/data/changes.json?updatedAt='+lastModified))
-        }
-        var modifiedSinceInitValue = Date.now();
-        var polling_frequency = 5000;
-        var initial_state = {modifiedSince: modifiedSinceInitValue, itemArray : []}
-        function max(property) {
-            return function (acc, current) {
-                acc = current[property] > acc ? current[property] : acc;
-            }
-        }
-        return Observable.return(initial_state)
-        .expand (function(state){
-            return fetchItem(state.modifiedSince)
-                .toArray()
-                .combineLatest(Observable.interval(polling_frequency).take(1), 
-                    function (itemArray, _) {
-                    return {
-                        modifiedSince : itemArray.reduce(max('updatedAt'), modifiedSinceInitValue), 
-                        itemArray : itemArray
-                    }
-                }
-              )
-            });
+  
+  constructor (name: string, frequency: number, action) {
+    var modifiedSinceInitValue = Date.now();
+    var polling_frequency = frequency;
+    var initial_state = {modifiedSince: modifiedSinceInitValue, itemArray : []}
+    function max(property) {
+      return function (acc, current) {
+        acc = current[property] > acc ? current[property] : acc;
+      }
     }
-    
+    return Observable.return(initial_state)
+      .expand (function(state){
+        return Rx.Observable.fromPromise(action(state.modifiedSince))
+          .toArray()
+          .combineLatest(Observable.interval(polling_frequency).take(1), 
+            function (itemArray, _) {
+            return {
+              modifiedSince : itemArray.reduce(max('updatedAt'), modifiedSinceInitValue), 
+              itemArray : itemArray
+            }
+          }
+        )
+      });
+  }
+  
   }
 
 }
